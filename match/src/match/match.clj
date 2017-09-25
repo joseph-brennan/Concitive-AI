70908e672917a89b6a70ca6304d82e5398a5f942
Joey <sharpshooter30@gmail.com>
2017-09-25 15:22:19 -0600

    working in a funcinal map-to-list

--- match/src/match/match.clj
+++ match/src/match/match.clj
@@ -2,4 +2,27 @@
   (:gen-class))
 
 (defn -main []
-  (println "Hello, World!"))
+  (defn header "returns the frame header" [frame] (first frame))
+  
+  (defn body "returns the frame body" [frame] (rest frame))
+  
+  (defn is-var?
+   "Determines if parameter is a variable"
+   [x]
+   (and (symbol? x)
+        (re-matches #"^\?.*" (name x))))
+  
+  (defn name-var
+    "Returns the name of a variable"
+    [x]
+    {:pre [(is-var? x)]}
+    (symbol (subs (name x) 1)))
+  
+  (defn match
+    "Determines whether the pattern matches the constant given the binding form;
+    returns updated binding form"
+    [pattern constant bindings])
+  
+  (defn map-to-alist
+    [x]
+    (map #(list (symbol(name (key %))) (val %)) (seq x))))
