0c12aed3c25d2730a24507e1c13ed34b54aa65b6
Joey <sharpshooter30@gmail.com>
2017-12-07 14:26:51 -0700

    try again tomorrow

--- NuralNet/NeuralNet.py
+++ NuralNet/NeuralNet.py
@@ -4,11 +4,18 @@ Joey Brennan
 
 
 class NeuralNet:
-    weight = []
-    forgetting = 0
 
     def __init__(self):
         pass
+
+    class Neurodes:
+        weight = 0
+        decay = 0
+        activation = 0
+
+        def __init__(self):
+            
+
 
 if __name__ == '__main__':
     pass
