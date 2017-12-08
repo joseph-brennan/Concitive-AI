80584284d072c37ea96d34b760227d7db802e55d
Joey <sharpshooter30@gmail.com>
2017-12-07 18:47:17 -0700

    making progress slowly for some reason looping infinitely

--- NuralNet/NeuralNet.py
+++ NuralNet/NeuralNet.py
@@ -1,21 +1,45 @@
 """
 Joey Brennan
 """
-
+import random
 
 class NeuralNet:
 
-    def __init__(self):
-        pass
-
     class Neurodes:
-        weight = 0
+        weight = {}
         decay = 0
         activation = 0
+        forgetting = 0
+        gain = 0
+        output = 0
 
-        def __init__(self):
-            
+        def __init__(self, decay, act, forget, gain):
+            self.decay = decay
+            self.activation = act
+            self.forgetting = forget
+            self.gain = gain
+
+            self.weight = {self}
+            self.output = random.random()
+
+        def making_weight(self, neuron):
+            for n in neuron:
+                self.weight[n].append(random.random())
+
+    def __init__(self, decay, act, forget, gain):
+        i = 0
+        self.brain = []
+        while i < 10:
+            neu = self.Neurodes
+            self.brain.append(neu(decay, act, forget, gain))
+
+    def printer(self):
+        print("tester")
+        for p in self.brain:
+            print(p)
 
 
 if __name__ == '__main__':
-    pass
+    print("bullshit")
+    work = NeuralNet(.5, .4, .3, .2)
+    work.printer()
